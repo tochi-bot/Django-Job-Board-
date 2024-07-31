385319cf5b3977a47b85ccb54d4a06e821bcb045
Anthony Nwadinobi <anthonynwadinobi@gmail.com>
2024-07-31 15:33:04 +0000

    added cp -r <Location>/allauth/templates/* ./templates/, migrations and debugging

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,6 +3,7 @@ from . import views  # Import views from the current module
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('jobs/<int:job_id>/', views.job_listing_detail, name='job_listing_detail'),
     path('jobs/', views.job_listing_detail, name='job_listing_detail'),
     path('job_listings/', views.job_listings, name='job_listings'),
     path('jobs/<int:job_id>/apply/', views.apply_for_job, name='apply_for_job'),

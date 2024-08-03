18b7515f76f15f9006d8ef9338ebb9dc4b14b3ce
Anthony Nwadinobi <anthonynwadinobi@gmail.com>
2024-08-03 13:43:57 +0000

    Deployement

--- jobs_platform/settings.py
+++ jobs_platform/settings.py
@@ -153,6 +153,7 @@ STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 
 MEDIA_URL = '/media/'
